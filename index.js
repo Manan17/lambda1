5af5f6ed4326935b6eb1e3c70cf2c458694f8722
Harsh Shah <80237556+ryuusama09@users.noreply.github.com>
2023-07-31 20:03:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(function (req, res, next) { 
+         res.header("Access-Control-Allow-Origin", "*"); 
+         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
+         next(); 
+ })
 //check this after completing all the functions, refactor the code :(
 const PORT = 3003;
 //console.log(process.env)
